c17656c1dc3628f0be9214ca43224973e5506830
Leonardo Schmitt <66500082+LeonardoSchmittK@users.noreply.github.com>
2025-06-25 21:52:03 -0300

    feat - clarity

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
+// src/App.js
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import ProtectedRoute from './components/ProtectedRoute';
+import { clarity } from 'react-microsoft-clarity';
 
 function App() {
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      clarity.init('s59v3im6b2'); // Substitua pelo seu Project ID do Clarity
+
+      // Exemplo: identificar usuário autenticado
+      // Troque 'USER_ID' pelo ID real do usuário, se disponível
+      // clarity.identify('USER_ID', { role: 'admin' });
+    }
+  }, []);
+
   return (
     <Router>
       <Routes>
-        {/* Public login page */}
+        {/* Página pública de login */}
         <Route path="/" element={<Login />} />
 
-        {/* Protected route for /Home */}
+        {/* Rotas protegidas */}
         <Route element={<ProtectedRoute />}>
-          <Route path="/Home" element={<Home />} />
+          <Route path="/home" element={<Home />} />
         </Route>
       </Routes>
     </Router>
